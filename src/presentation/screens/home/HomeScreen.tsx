b58c147992afdaceb0fa10771db4d0ce473699e4
Alferth Gonzalez <alferthgonzalez@Alferths-MacBook-Air.local>
2025-03-23 11:46:35 -0500

    creacion primera parte del menu

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,15 +1,94 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { globalStyles } from '../../../config/theme/theme';
+import { Title } from '../../components/ui/Title';
 
+
+export const menuItems = [
+  // 01-animationMenuItems
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    component: 'Animation102Screen',
+  },
+
+
+  // 02-menuItems
+  {
+    name: 'Pull to refresh',
+    icon: 'refresh-outline',
+    component: 'PullToRefreshScreen',
+  },
+  {
+    name: 'Section List',
+    icon: 'list-outline',
+    component: 'CustomSectionListScreen',
+  },
+  {
+    name: 'Modal',
+    icon: 'copy-outline',
+    component: 'ModalScreen',
+  },
+  {
+    name: 'InfiniteScroll',
+    icon: 'download-outline',
+    component: 'InfiniteScrollScreen',
+  },
+  {
+    name: 'Slides',
+    icon: 'flower-outline',
+    component: 'SlidesScreen',
+  },
+  {
+    name: 'Themes',
+    icon: 'flask-outline',
+    component: 'ChangeThemeScreen',
+  },
+
+  // 03- uiMenuItems
+  {
+    name: 'Switches',
+    icon: 'toggle-outline',
+    component: 'SwitchScreen',
+  },
+  {
+    name: 'Alerts',
+    icon: 'alert-circle-outline',
+    component: 'AlertScreen',
+  },
+  {
+    name: 'TextInputs',
+    icon: 'document-text-outline',
+    component: 'TextInputScreen',
+  },
+];
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <Icon name="home-outline" size={30} color="#000" />
-      <Ionicons name="home-outline" size={30} color="#000" />
+    <View style={[ globalStyles.mainContainer ]}>
+      <View style={ globalStyles.globalMargin }>
+        <ScrollView>
+          <Title text="Componente" safe />
+
+          {
+            menuItems.map( (item, index) => (
+              <View key={index}>
+                <Text>{item.name}</Text>
+                <Ionicons name={item.icon} size={50} color="black" />
+              </View>
+            ))
+          }
+
+        </ScrollView>
+      </View>
+
     </View>
   )
 }
